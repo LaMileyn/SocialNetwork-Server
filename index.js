0cc028d604f6aa98989117977779b2257e5948eb
Volg <lost-is-died@mail.ru>
2022-08-08 00:47:18 +0500

    recreated socket

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ const path = require("path")
 const router = require("./routes/index")
 const cors = require("cors")
 const errorMiddleWare = require("./middlewares/error-middleware")
+const initialyseSocket = require("./socket/index")
 dotenv.config()
 const app = express()
+
 
 app.use(cors())
 app.use(express.json())
@@ -41,7 +43,13 @@ app.post('/api/upload', upload.single('file'), (req, res,next) => {
 const start = async () => {
     try {
         await mongoose.connect(process.env.DB_CONN, { useNewUrlParser : true, useUnifiedTopology : true}, () => {})
-        app.listen(process.env.PORT || 5000, () => {})
+        const server = app.listen(process.env.PORT || 5000, () => {})
+        const io = require("socket.io")(server, {
+            cors: {
+                origin: "http://localhost:3000"
+            }
+        })
+        initialyseSocket(io)
     } catch (e) {
         console.log(e)
     }
--- socket/index.js
+++ socket/index.js
@@ -1,66 +1,63 @@
+const userService = require('./../services/user-service');
 
 
-
-const io = require("socket.io")(8900, {
-    cors: {
-        origin: "http://localhost:3000"
+module.exports = function initialyseSocket(io) {
+    let users = [];
+    const addNewUser = (userId, socketId) => !users.some(user => user.userId === userId) && users.push({userId, socketId})
+    const removeUser = (socketId) => {
+        users = users.filter(user => user.socketId !== socketId)
     }
-})
-let users = [];
-const addNewUser = (userId, socketId) => !users.some(user => user.userId === userId) && users.push({userId, socketId})
-const removeUser = (socketId) => {
-    users = users.filter(user => user.socketId !== socketId)
-}
-const getUser = (userId) => {
-    let res = users.find(user => {
-        return user.userId == userId
-    });
-    return res
-}
-io.on("connection", (socket) => {
-    socket.on("addUser", (userId) => {
-        // main list of users
-        addNewUser(userId, socket.id)
-        // creating special room for a user
-        socket.join(userId)
-        console.log("user joined: " + userId)
-        io.emit("getUsers", users)
-    })
-
-    socket.on("typing", async (room,userId) => {
-        // console.log("USER ID : " + userId)
-        // const user = await User.findById(userId);
-        //
-        // console.log(user)
-        socket.in(room).emit("typing",userId)
-    })
-    socket.on("stop-typing", (room,userId) => socket.in(room).emit("stop-typing",userId))
-
-    socket.on("join-room", async (room) =>{
-        // room equals to conversation._id //
-        socket.join(room);
-    })
-    socket.on("message-room",(data) =>{
-        const roomUsers = data.conversation.members;
-        roomUsers.forEach( roomUser => {
-            if( roomUser._id === data.sender.id) return;
-            socket.in(roomUser._id).emit("room-messages",data)
+    const getUser = (userId) => {
+        let res = users.find(user => {
+            return user.userId == userId
+        });
+        return res
+    }
+    io.on("connection", (socket) => {
+        socket.on("addUser", (userId) => {
+            // main list of users
+            addNewUser(userId, socket.id)
+            // creating special room for a user
+            socket.join(userId)
+            console.log("user joined: " + userId)
+            io.emit("getUsers", users)
         })
-    });
 
-    socket.on("disconnect", () => {
-        removeUser(socket.id)
-        io.emit("getUsers", users)
+        socket.on("typing", async (room,userId) => {
+            const user = await userService.getOneUser(userId)
+            socket.in(room).emit("typing",user)
+        })
+        socket.on("stop-typing", (room,userId) => {
+            console.log("stop")
+            socket.in(room).emit("stop-typing",userId)
+        })
+
+        socket.on("join-room", async (room) =>{
+            // room equals to conversation._id //
+            socket.join(room);
+        })
+        socket.on("message-room",(data) =>{
+            const roomUsers = data.conversation.members;
+            roomUsers.forEach( roomUser => {
+                if( roomUser._id === data.sender.id) return;
+                socket.in(roomUser._id).emit("room-messages",data)
+            })
+        });
+
+        socket.on("disconnect", () => {
+            removeUser(socket.id)
+            io.emit("getUsers", users)
+        })
+
+        // socket.on("invite-to-room", async (room,myId,userId) =>{
+        //     let currentConversation = await conversationService.getOneConversation(room);
+        //     const user = getUser(userId)
+        //     user && io.to(user.socketId).emit("send-invite-to-room",currentConversation);
+        //     socket.broadcast.emit("new-room-member", {
+        //         invitor : myId,
+        //         invitedPerson : userId,
+        //     });
+        // })
+
     })
-
-    // socket.on("invite-to-room", async (room,myId,userId) =>{
-    //     let currentConversation = await conversationService.getOneConversation(room);
-    //     const user = getUser(userId)
-    //     user && io.to(user.socketId).emit("send-invite-to-room",currentConversation);
-    //     socket.broadcast.emit("new-room-member", {
-    //         invitor : myId,
-    //         invitedPerson : userId,
-    //     });
-    // })
-
-})
+}
